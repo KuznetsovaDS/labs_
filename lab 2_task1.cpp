fd24e43d5865e23ae567a05a6a2efa1cb2177f3c
Darya Kuznetsova <Dashar1996@gmail.com>
2024-07-15 23:39:47 +0300

    Add files via upload

---
+++ lab 2_task1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	system("chcp 1251");
+	double x, y;
+	cout << "Введите координаты x y ";
+	cin >> x >> y;
+	double radiusSq = x * x + y * y;
+	double radius = 3.0;
+	double rad = radius * radius;
+	if (radiusSq < rad && y >0) {
+		cout << "внутри" << endl;
+	}
+	else if (radiusSq > rad || y < 0)
+	{
+		cout << "снаружи" << endl;
+	}
+	else {
+		cout << "на границе" << endl;
+	}
+}
