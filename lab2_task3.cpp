dc1606db2942d490ab6c70d4cbfd89ee21496e49
Darya Kuznetsova <Dashar1996@gmail.com>
2024-07-16 00:24:44 +0300

    Add files via upload

---
+++ lab2_task3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+void minCoin(int amount) {
+	int c10 = amount / 10;
+	amount %= 10;
+    int c5 = amount / 5;
+    amount %= 5;
+    int c2 = amount / 2;
+    amount %= 2;
+    int c1 = amount;
+    cout << "10р " << c10 << " монет" << endl;
+    cout << "5р " << c5 << " монет" << endl;
+    cout << "2р " << c2 << " монет" << endl;
+    cout << "1р " << c1 << " монет" << endl;
+}
+int main() {
+    system("chcp 1251");
+    int amount;
+    cout << "Введите сумму: ";
+    cin >> amount;
+    minCoin(amount);
+}
