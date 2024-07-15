43653d075ebef0ce7c1359969f48c7d861ca2ca0
Darya Kuznetsova <Dashar1996@gmail.com>
2024-07-16 00:05:24 +0300

    Add files via upload

---
+++ lab2_task1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+bool isSimp(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+    return true;
+
+}
+bool isSupSimp(int n) {
+    while (n > 0) {
+        if (!isSimp(n)) return false;
+        n /= 10;
+    }
+    return true;
+}
+int main() {
+    system("chcp 1251");
+    int num;
+    cout << "Введите число ";
+    cin >> num;
+    if (isSupSimp(num)) {
+        cout << num << " суперпростое\n";
+
+    }
+    else {
+        cout << num << " несуперпростое\n";
+    }
+
+}
