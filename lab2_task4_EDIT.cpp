e533d5dc98adae06249b2f5f6d11d9a35a2ea78e
Darya Kuznetsova <Dashar1996@gmail.com>
2024-07-16 20:12:01 +0300

    Add files via upload

---
+++ lab2_task4_EDIT.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+using namespace std;
+string shootLevel(int points) {
+    if (points >= 50) {
+        return "Снайпер";
+    }
+    else if (points >= 30) {
+        return "Опытный стрелок";
+    }
+    else if (points >= 10) {
+        return "Новичок";
+    }
+    else {
+        return "Начинающий";
+    }
+    }
+int main()
+{
+    system("chcp 1251");
+    const int score = 50;
+    int totalPoints = 0;
+    int shots = 0;
+    while (totalPoints < score) {
+        int x, y;
+        cout << "Введите координаты x y ";
+        cin >> x >> y;
+        double dist = sqrt(x * x + y * y);
+        if (dist <= 1) {
+            totalPoints += 10;
+        }
+        else if (dist <= 2) {
+            totalPoints += 5;
+        }
+        shots++;
+    }
+    
+    cout << "Набрано " << totalPoints << " очков за " << shots << " выстрелов" <<endl;
+    string level = shootLevel(totalPoints);
+    cout << "уровень " << level << endl;
+}
+
